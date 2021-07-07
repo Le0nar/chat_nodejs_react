0be54f275202da19c7d151c9b2d306360c3630f2
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-07-07 11:25:24 +0400

    feat: add users module

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const PORT = process.env.pORT || 5000;
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(PORT, () => console.log(`Server started on port ${PORT}`)
+    )
 }
-bootstrap();
+
+bootstrap()
