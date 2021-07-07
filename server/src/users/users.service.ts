6b0caf7a01a3b89919d30265278c1e87c012b7d3
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-07-07 12:27:20 +0400

    feat: add registration

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -5,15 +5,24 @@ import { User } from './users.model';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User) private userRepository: typeof User ) {}
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
 
   async createUser(dto: CreateUserDto) {
-      const user = await this.userRepository.create(dto);
-      return user;
+    const user = await this.userRepository.create(dto);
+    return user;
   }
 
   async getAllUsers() {
     const users = await this.userRepository.findAll();
     return users;
+  }
+
+  async getUserByEmail(email: string) {
+    const user = await this.userRepository.findOne({
+      where: { email },
+      include: { all: true },
+    });
+
+    return user;
   }
 }
